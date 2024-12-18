901521c57c510e60e0a87f6151822a87189f5c55
sharmaishan74481@gmail.com <sharmaishan74481@gmail.com>
2024-12-18 15:34:59 +0530

    Add feature for search

--- src/components/main/general/GeneralButton.jsx
+++ src/components/main/general/GeneralButton.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../../index.css";
+import '../../../index.css';
 
 const GeneralButton = ({text,statusBar,custom}) => {
   const customer=['Add','Search','Update','Deactivate']
